94417cb7113eb867c5986836746a308b97f02016
Younus Ali Akash <younusaliakash8@gmail.com>
2021-12-17 01:00:17 +0600

    [Routes setup]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Components/Common/Header';
+const Help = React.lazy(() => import('./Pages/Help/Help'));
+const Home = React.lazy(() => import('./Pages/Home/Home'));
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React App</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/help" element={<Help />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
