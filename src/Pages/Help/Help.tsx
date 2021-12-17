3ba7848cf96b04de87ca709811b2d4229cdccb31
Younus Ali Akash <younusaliakash8@gmail.com>
2021-12-17 16:37:41 +0600

    [Sass Inplement]

--- src/Pages/Help/Help.tsx
+++ src/Pages/Help/Help.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Help = () => {
   return (
     <div>
-      <h1>This is help page</h1>
+      <h1 className="text-primary">This is help page</h1>
     </div>
   );
 };
